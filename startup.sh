e8ab293cb444d7c72e3ba8a77f4b1082d59dd469
keigooba <keigo@MacBook-Pro.local>
2020-08-08 22:40:24 +0900

    startup.sh change

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 cp .env.example .env
 composer install
-touch /var/www/myapp/storage/logs/laravel.log
-chmod -R 777 /var/www/myapp/storage
-chmod -R 777 /var/www/myapp/bootstrap
+touch /var/www/main_attendance/storage/logs/laravel.log
+chmod -R 777 /var/www/main_attendance/storage
+chmod -R 777 /var/www/main_attendance/bootstrap
 php artisan key:generate
 php artisan migrate
 php artisan db:seed
