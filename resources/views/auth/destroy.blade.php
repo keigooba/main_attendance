48124bd35ecf6e946a2c93ac262673ea7cef5a56
keigooba <keigo2356@icloud.com>
2020-08-29 09:49:31 +0900

    header change

--- resources/views/auth/destroy.blade.php
+++ resources/views/auth/destroy.blade.php
@@ -13,7 +13,8 @@
       <div class="col-12 text-center">
         <p><span class="font-weight-bold">{{Auth::user()->name}}</span>を本当に削除しますか？</p>
         <p class="text-danger"><span class="font-weight-bold">{{Auth::user()->name}}</span>の全ての情報が削除されます。</p>
-        <p class="text-danger">再度、同じ名前・メールアドレスでは登録できません。</p>
+        <p class="text-danger">再度、同じ名前・メールアドレス<br>
+        では登録できません。</p>
         <input type="hidden" name="admin" value="{{ Auth::user()->id }}">
         <input type="submit" name="submit" value="削除" class="btn btn-dark" style="border-radius: initial; float: initial">
       </div>
